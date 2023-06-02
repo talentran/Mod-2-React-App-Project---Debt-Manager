811d70350de9e144cd962345ee6c7f52668bde8a
Tri Tran <ttran@LABA.PSWAS.LOCAL>
2023-06-01 17:52:36 -0700

    working on app function

--- src/App.js
+++ src/App.js
@@ -7,48 +7,59 @@ import "./App.css";
 import { renderResultAmount } from "./helpers/renderResultAmount";
 
 function App() {
-  const [userName, setUserName] = useState("");
+  const [userName] = useState("Tri");
   const [friendName, setFriendName] = useState("");
   const [amount, setAmount] = useState(0);
+
   const [loanedAmount, setLoanedAmount] = useState(0);
   const [borrowedAmount, setBorrowedAmount] = useState(0);
+
   const [users, setUsers] = useState({});
+  const [currency, setCurrency] = useState("USD");
+  const [partnerLocation, setPartnerLocation] = useState("");
+
   const [isSignUp, setIsSignUp] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const [history, setHistory] = useState([]);
-
-  // const obj = {
-  //   from: 'Tri',
-  //   to: 'Vitalii',
-  //   amount: -500,
-  //   date: Date.now()
-  // }
+  const [receipts, setReceipts] = useState([]);
 
   useEffect(() => {
     fetch('http://worldtimeapi.org/api/timezone/PST8PDT').then((res) => res).then((r) => r.json()).then((d) => console.log(d))
   }, [])
 
   const onLoanClick = () => {
-    setLoanedAmount((prevLoanedAmount) => prevLoanedAmount + amount);
+    const newLoanedAmount = loanedAmount + amount;
+    setLoanedAmount(newLoanedAmount);
 
     const transaction = {
       from: userName,
       to: friendName,
-      amount: amount
-    }
-    setHistory([...history, transaction])
+      amount: amount,
+      currency: currency,
+      location: "USA",
+      time: new Date().toLocaleString()
+    };
 
+    const newReceipt = `Receipt: User ${transaction.from} in ${transaction.location} has lent ${transaction.amount} ${transaction.currency} to ${transaction.to} in ${partnerLocation} at ${transaction.time}. Total lent so far: ${newLoanedAmount} ${transaction.currency}`;
+    console.log(newReceipt);
+    setReceipts([...receipts, newReceipt]);
   };
 
   const onBorrowClick = () => {
-    setBorrowedAmount((prevBorrowedAmount) => prevBorrowedAmount + amount);
+    const newBorrowedAmount = borrowedAmount + amount;
+    setBorrowedAmount(newBorrowedAmount);
+
     const transaction = {
-      from: userName,
-      to: friendName,
-      amount: amount
-    }
-    setHistory([...history, transaction])
+      from: friendName,
+      to: userName,
+      amount: amount,
+      currency: currency,
+      location: partnerLocation,
+      time: new Date().toLocaleString()
+    };
+    
+    const receipt = `Receipt: ${transaction.from} at ${transaction.location} lent ${transaction.amount} ${transaction.currency} to ${transaction.to} in USA on ${transaction.time}. Total borrowed so far: ${newBorrowedAmount} ${transaction.currency}`;
+    setReceipts(prevReceipts => [...prevReceipts, receipt]);
   };
 
   const result = loanedAmount - borrowedAmount;
@@ -64,7 +75,7 @@ function App() {
   }, [result]);
 
   const handleLogin = (userID, password) => {
-    if (userID === 'Tri' && password === 'password') {
+    if (userID === 'Tri' && password === 'admin') {
       setIsLoggedIn(true);
     } else {
       alert('Sorry, in this version only Tri is allowed to login');
@@ -84,48 +95,62 @@ function App() {
     if (isSignUp) {
       return <SignUpForm onSignUp={handleSignUp} />;
     } else {
-      return <LoginForm onLogin={handleLogin} onSignUpClick={() => setIsSignUp(true)} />;
+      return (
+          <LoginForm onLogin={handleLogin} onSignUpClick={() => setIsSignUp(true)} />
+      )
     }
   }
   
-
   return (
     <div className="App">
-      <p>Welcome, admin!</p>
+      <p>Please add your client or partner</p>
+      
       <Input
-        placeholder="Your name"
-        onChange={(e) => setUserName(e.target.value)}
-      />
-      <Input
-        placeholder="Your partner"
+        placeholder="Name of your client or partner"
         onChange={(e) => setFriendName(e.target.value)}
       />
+
+      <select onChange={(e) => setCurrency(e.target.value)}>
+        <option value="USD">US Dollar (USD)</option>
+        <option value="VND">Vietnamese Dong (VND)</option>
+        <option value="RUB">Russian Ruble (RUB)</option>
+        <option value="INR">Indian Rupee (INR)</option>
+        <option value="GOLD">GOLD (1 oz)</option>
+      </select>
+
       <Input
+        placeholder="Location of your client or partner"
+        onChange={(e) => setPartnerLocation(e.target.value)}
+      />
+
+      <Input  
         placeholder="Amount"
         type="number"
         onChange={(e) => setAmount(Number(e.target.value))}
       />
-      <p>User: {userName}</p>
-      <p>Partner: {friendName}</p>
-      <p>Money: ${amount}</p>
+
       <Button
         text={`Lend ${friendName} money`}
         color="green"
         onClick={onLoanClick}
       />
+
       <Button
         text={`Borrow ${friendName} money`}
         color="red"
         onClick={onBorrowClick}
       />
+
+      <p><u>Summary:</u></p>
+      <p>Partner: {friendName}</p>
+      <p>Money: ${amount}</p>
       {renderResultAmount(result)}
-      {history.map((item, index) => (
-        <div key={index} style={{border: '1px solid black', marginTop: 10}}>
-          <p>From: {item.from}</p>
-          <p>To: {item.to}</p>
-          <p>Amount: {item.amount}</p>
-        </div>
+
+      {receipts.map((receipt, index) => (
+        <p key={index}>{receipt}</p>
       ))}
+      {/* <History history={history} /> */}
+
     </div>
   );
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FaGoogle, FaFacebookF, FaMicrosoft, FaAmazon, FaPaypal } from "react-icons/fa";
 import './LoginForm.css';
 
 const LoginForm = ({ onLogin, onSignUpClick }) => {
@@ -17,19 +18,54 @@ const LoginForm = ({ onLogin, onSignUpClick }) => {
       <form onSubmit={handleSubmit}>
         <label>
           Your ID:
-          <input type="text" value={userID} onChange={e => setUserID(e.target.value)} />
+            <input type="text" value={userID} onChange={e => setUserID(e.target.value)} />
         </label>
         <label>
           Password:
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
         </label>
+
+        <div className="remember-me">
+            <input type="checkbox" id="remember-me-checkbox" />
+            <label htmlFor="remember-me-checkbox">Remember me</label>
+        </div>
+
+        <a href="#future-feature" className="forgot-password">Forgot your password?</a>
+
         <button type="submit" className="login-btn">Login</button>
-      </form>
-      <br></br>
-      <label>
-        Register for an account:
-      </label>
-      <button className="signup-btn" onClick={onSignUpClick}>Sign Up</button>
+        </form>
+
+        <p>Or login with:</p>
+
+        <button className="social-login-btn google">
+            <FaGoogle />
+            Log in with Google
+        </button>
+
+        <button className="social-login-btn facebook">
+            <FaFacebookF />
+            Log in with Facebook
+        </button>
+
+        <button className="social-login-btn microsoft">
+            <FaMicrosoft />
+            Log in with Microsoft
+        </button>
+
+        <button className="social-login-btn amazon">
+            <FaAmazon />
+            Log in with Amazon
+        </button>
+
+        <button className="social-login-btn paypal">
+            <FaPaypal />
+            Log in with PayPal
+        </button>
+        <br></br>
+        <label>
+            Register for an account:
+        </label>
+        <button className="signup-btn" onClick={onSignUpClick}>Sign Up</button>
     </div>
   );
 };
